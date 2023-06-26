689f36a2dca44322bfdec9783d67316a70e68742
VolodymyrBodniak <bodnjak@gmail.com>
2023-06-26 19:10:44 +0200

    update task-06

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputElement = document.getElementById("validation-input");
+
+inputElement.addEventListener("blur", () => {
+  const inputValue = inputElement.value.trim();
+  const expectedLength = parseInt(inputElement.getAttribute("data-length"));
+
+  if (inputValue.length === expectedLength) {
+    inputElement.classList.remove("invalid");
+    inputElement.classList.add("valid");
+  } else {
+    inputElement.classList.remove("valid");
+    inputElement.classList.add("invalid");
+  }
+});
