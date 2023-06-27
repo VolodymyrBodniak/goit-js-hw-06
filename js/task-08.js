65ebe43275b125a4dfa5fe6116597b187f10a48c
VolodymyrBodniak <bodnjak@gmail.com>
2023-06-27 20:55:53 +0400

    update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Please, enter all form");
+  } else {
+    const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+    loginForm.reset();
+  }
+});
