54f85d7292c0c0f8047f0bdd07f52a74484f32cb
VolodymyrBodniak <bodnjak@gmail.com>
2023-07-01 22:18:11 +0400

    Corrected task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const fontSizeControl = document.getElementById("font-size-control");
 const textElement = document.getElementById("text");
 
+fontSizeControl.value = parseInt(getComputedStyle(textElement).fontSize);
+
 fontSizeControl.addEventListener("input", () => {
   const fontSize = fontSizeControl.value + "px";
   textElement.style.fontSize = fontSize;
