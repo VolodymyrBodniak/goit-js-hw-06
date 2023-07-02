0be86bb9b1c98f23fb18a3483ae0bec9e3ab571b
VolodymyrBodniak <bodnjak@gmail.com>
2023-07-02 22:25:54 +0400

    corrected task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,33 +1,19 @@
-// const categoriesList = document.querySelector("#categories");
+const categoriesList = document.querySelector("#categories");
 
-// const categoryItems = categoriesList.querySelectorAll("li.item");
+const categoryItems = categoriesList.querySelectorAll("li.item");
 
-// const numberOfCategories = categoryItems.length;
-// console.log(`Number of categories: ${numberOfCategories}`);
+const numberOfCategories = categoryItems.length;
+console.log(`Number of categories: ${numberOfCategories}`);
 
-// categoryItems.forEach((item) => {
-//   const categoryTitle = item.querySelector("h2").textContent;
+categoryItems.forEach((item) => {
+  const categoryTitle = item.firstElementChild.textContent;
 
-//   const elements = item.querySelectorAll("li");
+  const elements = Array.from(item.children).filter(
+    (child) => child.tagName === "li"
+  );
 
-//   const numberOfElements = elements.length;
-
-//   console.log(`Category: ${categoryTitle}`);
-//   console.log(`Elements: ${numberOfElements}`);
-// });
-
-const categoriesList = document.getElementById("categories");
-const categoryItems = categoriesList.children;
-
-console.log(`Number of categories: ${categoryItems.length}`);
-
-for (let i = 0; i < categoryItems.length; i += 1) {
-  const categoryItem = categoryItems[i];
-  const categoryTitle = categoryItem.querySelector("h2").textContent;
-
-  const elements = categoryItem.querySelectorAll("li");
   const numberOfElements = elements.length;
 
   console.log(`Category: ${categoryTitle}`);
   console.log(`Elements: ${numberOfElements}`);
-}
+});
